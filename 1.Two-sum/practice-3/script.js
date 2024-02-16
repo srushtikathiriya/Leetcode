618f8ded9b5b6cdc87774cbde49cd55b9b6afb94
srushti <srushtikathiriya2003@gmail.com>
2024-02-16 21:18:11 +0530

    16-02-2024

--- 1.Two-sum/practice-3/script.js
+++ 1.Two-sum/practice-3/script.js
@@ -11,7 +11,7 @@ function twoSum(arr, target) {
     let n = arr.length;
     for (let i = 0; i < n; i++) {
         for (let j = i + 1; j < n; j++) {
-            if (arr[i] + arr[j] === target)  //checks if the sum of the current pair of elements is equal to the target sum
+            if (arr[i] + arr[j] === target)  
              {
                 return [i, j];
             }
