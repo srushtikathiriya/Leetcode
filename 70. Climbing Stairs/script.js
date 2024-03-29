eb51fe46491e8412fbb791127cd03282eb8c6771
srushti <srushtikathiriya2003@gmail.com>
2024-03-29 17:18:34 +0530

    29-03-2024

---
+++ 70. Climbing Stairs/script.js
@@ -0,0 +1,46 @@
+// You are climbing a staircase. It takes n steps to reach the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+// Example 1:
+
+// Input: n = 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+
+
+var climbStairs = function(n) {
+    if(n==0 || n==1) {
+        return 1
+    }
+    if(n<0){
+        return 0
+    } 
+    return climbStairs(n-1) + climbStairs(n-2)
+};
+console.log(climbStairs(2));
+
+
+// Example 2:
+
+// Input: n = 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+var climbStairs = function(n) {
+    if(n==0 || n==1){
+        return 1;
+    } 
+    if(n<0) {
+        return 0;
+    }
+    return climbStairs(n-1) + climbStairs(n-2)
+};
+console.log(climbStairs(3));
